b73b6c14ab8acb89ec59b4ec7978c24003021d96
Jards Guimaraes <jardsguimaraes@hotmail.com>
2025-05-06 12:18:08 -0300

    Implementado o delete cascade para as tabelas

--- course/src/main/java/com/ead/course/services/CourseService.java
+++ course/src/main/java/com/ead/course/services/CourseService.java
@@ -1,8 +1,45 @@
 package com.ead.course.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ead.course.model.CourseModel;
+import com.ead.course.model.LessonModel;
+import com.ead.course.model.ModuleModel;
+import com.ead.course.repository.CourseReporitory;
+import com.ead.course.repository.LessonRepository;
+import com.ead.course.repository.ModuleRepository;
 
 @Service
 public class CourseService {
-    
+
+    @Autowired
+    private CourseReporitory courseReporitory;
+
+    @Autowired
+    private ModuleRepository moduleRepository;
+
+    @Autowired
+    private LessonRepository lessonRepository;
+
+    @Transactional
+    public void delete(CourseModel courseModel) {
+        List<ModuleModel> moduleModelsList = moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
+
+        if (!moduleModelsList.isEmpty()) {
+            for (ModuleModel module : moduleModelsList) {
+                List<LessonModel> lessonModuleList = lessonRepository.findAllLessonsIntoModule(module.getModuleId());
+
+                if (!lessonModuleList.isEmpty()) {
+                    lessonRepository.deleteAll(lessonModuleList);
+                }
+            }
+            moduleRepository.deleteAll(moduleModelsList);
+        }
+
+        courseReporitory.delete(courseModel);
+    }
 }
