c3a0abe3574e1c972607b4e1bda4adaf2ae95ce6
Jards Guimaraes <jardsguimaraes@hotmail.com>
2025-05-13 09:12:46 -0300

    Implementado ISO-8601-UTC de forma global nos projetos authuser e course

---
+++ course/src/main/java/com/ead/course/configs/DateConfig.java
@@ -0,0 +1,23 @@
+package com.ead.course.configs;
+
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+@Configuration
+public class DateConfig {
+
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> {
+            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)));
+        };
+    }
+
+}
